c81822467bf5a189350cd4431f67464f7edfc2c2
NonozgYtb <nonozg.ytb@gmail.com>
2020-10-06 18:31:35 +0200

    Init

---
+++ src/Exception/DindentException.php
@@ -0,0 +1,10 @@
+<?php
+namespace NonozgYtb\Dindent\Exception;
+
+/**
+ * @link https://github.com/nonozgytb/dintent for the canonical source repository
+ * @license https://github.com/nonozgytb/dintent/blob/master/LICENSE BSD 3-Clause
+ */
+class DindentException extends \Exception {
+
+}
